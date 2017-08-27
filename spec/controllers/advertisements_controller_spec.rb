d85b5ba2dadee4e0c83a9508f674b75950bc9e05
Corey <cdekk3r@hotmail.com>
2017-08-27 22:03:12 +0000

    post index

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -2,34 +2,21 @@ require 'rails_helper'
 
 RSpec.describe AdvertisementsController, type: :controller do
   
-  let(:my_ad) { Advertisement.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(1..100)) }
+  let(:my_ad) { Advertisement.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100) }
 
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+    
+    it "assigns [my_post] to @posts" do
+      get :index
+      
+      expect(assigns(:advertisements)).to eq([my_ad])
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
+  
 
 end
