5135083426d573360c1baefa1e403708d309f351
Corey <cdekk3r@hotmail.com>
2017-08-28 23:52:30 +0000

    new create edit update tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,8 +8,39 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+    
+    if @question.save
+      flash[:notice] = "Question was saved."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving your question. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @question = Question.find(params[:id])
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
   end
 end
