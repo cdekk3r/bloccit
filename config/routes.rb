8283985c3db3bbefde4f0573ec5f458137e8f5ec
Corey <cdekk3r@hotmail.com>
2017-08-28 22:55:07 +0000

    question index and show tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
+  resources :questions
   resources :advertisements
   resources :posts
 
   get 'about' => 'welcome#about'
-  
-  get 'welcome/contact'
-  
-  get 'welcome/faq'
   
   root 'welcome#index'
   
