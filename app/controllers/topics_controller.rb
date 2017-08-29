581b5861ba90fc9d891ee7bc1d674c24a1c9dfa0
Corey <cdekk3r@hotmail.com>
2017-08-29 21:50:20 +0000

    added new create edit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,29 @@ class TopicsController < ApplicationController
        @topics = Topic.all 
     end
     
-   def show
-     @topic = Topic.find(params[:id])
-   end
+    def show
+       @topic = Topic.find(params[:id])
+    end
+   
+    def new
+       @topic = Topic.new
+    end
+    
+    def create
+        @topic = Topic.new
+        @topic.name = params[:topic][:name]
+        @topic.description = params[:topic][:description]
+        @topic.public = params[:topic][:public]
+
+        if @topic.save
+            redirect_to @topic, notice: "Topic was saved successfully."
+        else
+            flash.now[:alert] = "Error creating topic. Please try again."
+            render :new
+        end
+    end
+    
+    def edit
+        @topic = Topic.find(params[:id])
+    end
 end
